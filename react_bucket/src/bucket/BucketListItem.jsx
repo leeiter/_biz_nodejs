99d6e469a808838c4fd5fc9c4c9f8826f58f1929
leeiter <iterlees@gmail.com>
2020-03-25 13:39:35 +0900

    Bucket Project

---
+++ react_bucket/src/bucket/BucketListItem.jsx
@@ -0,0 +1,131 @@
+import React, { Component } from "react";
+
+class BucketListItem extends Component {
+  state = {
+    isEditing: false,
+    b_title: "",
+    b_checked: false
+  };
+
+  inputClick = ev => {
+    ev.stopPropagation();
+  };
+
+  toggleEdit = () => {
+    const { isEditing } = this.state;
+    this.setState({
+      isEditing: !isEditing
+    });
+  };
+
+  editInput = ev => {
+    this.setState({
+      ...this.state,
+      b_title: ev.target.value
+    });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { bucket } = this.props;
+    if (!prevState.isEditing && this.state.isEditing) {
+      this.setState({
+        b_title: bucket.b_title
+      });
+    }
+  }
+
+  updateHandle = () => {
+    const { bucket, bucket_url } = this.props;
+    const data = { _id: bucket._id, b_title: this.state.b_title };
+    fetch(bucket_url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+  };
+
+  updateCheckHandle = ev => {
+    const { bucket, bucket_url } = this.props;
+    const data = { _id: bucket._id, b_checked: this.state.b_checked };
+    fetch(bucket_url + "/check", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+    ev.stopPropagation();
+  };
+
+  deleteHandle = ev => {
+    ev.stopPropagation();
+
+    if (window.confirm("데이터를 삭제할까요?")) {
+      const { bucket, bucket_url } = this.props;
+      const data = { _id: bucket._id };
+      fetch(bucket_url, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      });
+    }
+  };
+
+  render() {
+    const { bucket } = this.props;
+    return (
+      <tr data-id={bucket._id}>
+        <td>{bucket.b_date}</td>
+        <td>{bucket.b_time}</td>
+        <td>
+          <div>
+            <input
+              type="checkbox"
+              onClick={this.updateCheckHandle}
+              value={`${
+                bucket.b_checked !== bucket.b_checked
+                  ? !bucket.b_checked
+                  : bucket.b_checked
+              }`}
+            />
+          </div>
+        </td>
+        <td onClick={this.toggleEdit}>
+          {this.state.isEditing ? (
+            <div>
+              <input
+                value={this.state.b_title}
+                onClick={this.inputClick}
+                onChange={this.editInput}
+              />
+              <button
+                type="button"
+                onClick={this.updateHandle}
+                className="w3-button w3-orange w3-tiny"
+              >
+                완료
+              </button>
+            </div>
+          ) : (
+            <span>{bucket.b_title}</span>
+          )}
+        </td>
+        <td>
+          <button
+            type="button"
+            onClick={this.deleteHandle}
+            className="w3-button w3-red w3-small"
+          >
+            삭제
+          </button>
+        </td>
+      </tr>
+    );
+  }
+}
+
+export default BucketListItem;
